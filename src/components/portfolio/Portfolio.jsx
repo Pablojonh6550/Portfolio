a6e80aa1cf83ac13224db97b1cbb79692c4f30b5
Pablojonh6550 <pablojonh6550@gmail.com>
2022-04-17 11:38:16 -0300

    version 1.6

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,61 +4,12 @@ import React from "react";
 import './css/portfolio.css';
 // --Card--
 import PortfolioCard from "./PortfolioCard";
-
-// --Images--
-import IMG1 from '../../assets/portfolio1.jpg';
-import IMG2 from '../../assets/portfolio2.jpg';
-import IMG3 from '../../assets/portfolio3.jpg';
-import IMG4 from '../../assets/portfolio4.jpg';
-import IMG5 from '../../assets/portfolio5.png';
-import IMG6 from '../../assets/portfolio6.jpg';
-
-const data = [
-    {
-        id: 1,
-        image: IMG1,
-        title: 'Project1',
-        github_link:'http://github.com',
-        demo:'http://dribble.com',
-    },
-    {
-        id: 2,
-        image: IMG2,
-        title: 'Project2',
-        github_link:'http://github.com',
-        demo:'http://dribble.com',
-    },
-    {
-        id: 3,
-        image: IMG3,
-        title: 'Project3',
-        github_link:'http://github.com',
-        demo:'http://dribble.com',
-    },
-    {
-        id: 4,
-        image: IMG4,
-        title: 'Project4',
-        github_link:'http://github.com',
-        demo:'http://dribble.com',
-    },
-    {
-        id: 5,
-        image: IMG5,
-        title: 'Project5',
-        github_link:'http://github.com',
-        demo:'http://dribble.com',
-    },
-    {
-        id: 6,
-        image: IMG6,
-        title: 'Project6',
-        github_link:'http://github.com',
-        demo:'http://dribble.com',
-    },
-]
+// --Dados--
+import {datas} from './data';
 
 function Portfolio() {
+    const datap = datas;
+
     return (
         <>
         <section id='portfolio'>
@@ -66,10 +17,11 @@ function Portfolio() {
             <h2>Portfolio</h2>
             <div className="container portfolio_container"> 
             {
-            data.map(({id, image, title, github, demo}) => {
-                return(
-                    <PortfolioCard id={id} image={image} title={title} github={github} demo={demo} />
-                )
+                datap.map((data) => {
+                    return(
+                        <PortfolioCard id={data.id} image={data.image} title={data.title} github={data.github} demo={data.demo} />
+                   
+                    )
             })
             }     
             </div>
---
+++ src/components/portfolio/data.js
@@ -0,0 +1,57 @@
+// --Images--
+import IMG1 from '../../assets/portfolio1.jpg';
+import IMG2 from '../../assets/portfolio2.jpg';
+import IMG3 from '../../assets/portfolio3.jpg';
+import IMG4 from '../../assets/portfolio4.jpg';
+import IMG5 from '../../assets/portfolio5.png';
+import IMG6 from '../../assets/portfolio6.jpg';
+
+
+export const datas = [
+    {
+        id: 1,
+        image: IMG1,
+        title: 'Project1',
+        github_link:'http://github.com',
+        demo:'http://dribble.com',
+    },
+    {
+        id: 2,
+        image: IMG2,
+        title: 'Project2',
+        github_link:'http://github.com',
+        demo:'http://dribble.com',
+    },
+    {
+        id: 3,
+        image: IMG3,
+        title: 'Project3',
+        github_link:'http://github.com',
+        demo:'http://dribble.com',
+    },
+    {
+        id: 4,
+        image: IMG4,
+        title: 'Project4',
+        github_link:'http://github.com',
+        demo:'http://dribble.com',
+    },
+    {
+        id: 5,
+        image: IMG5,
+        title: 'Project5',
+        github_link:'http://github.com',
+        demo:'http://dribble.com',
+    },
+    {
+        id: 6,
+        image: IMG6,
+        title: 'Project6',
+        github_link:'http://github.com',
+        demo:'http://dribble.com',
+    },
+]
+
+export default {
+    datas,
+}
